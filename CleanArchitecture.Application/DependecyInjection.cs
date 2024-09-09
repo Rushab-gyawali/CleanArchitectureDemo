4d06479700689b3e20ceaef1acf24e380cb65f12
Rushab Gyawali <gyawali.rushab@gmail.com>
2024-09-09 23:33:41 +0545

    Add project files.

---
+++ CleanArchitecture.Application/DependecyInjection.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Application
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = typeof(DependecyInjection).Assembly;
+            services.AddMediatR(configuration =>
+            configuration.RegisterServicesFromAssembly(
+                typeof(DependecyInjection).Assembly));
+
+            services.AddValidatorsFromAssembly(assembly);
+
+            return services;
+        }
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,25 @@
+using CleanArchitecture.Application;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication().AddInfracture().AddPresentation();
+
+//need to implement in program.js
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.Run();
